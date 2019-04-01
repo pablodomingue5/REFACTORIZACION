7f2967ccfe0e87e680f4cd3005d3016bf0b9b68a
Pablo Dominguez <pablod.s66@gmail.com>
2019-04-01 12:21:19 +0200

    V2

--- ED REFACTORIZACION/src/es/Studium/Refactorizacion/Refactorizacion.java
+++ ED REFACTORIZACION/src/es/Studium/Refactorizacion/Refactorizacion.java
@@ -1,5 +1,7 @@
 package es.Studium.Refactorizacion;
 
 public class Refactorizacion {
-
+	public double calcularPrecio(int unidades,double preciounitario) {
+		return(unidades*preciounitario);
+	}
 }
